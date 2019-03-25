677104d9e837c3e44da30b208057b2b38bb8ba91
Jean Carlos Contreras <jeancon29@gmail.com>
2019-03-25 13:12:16 -0400

    Se modifico archivo UsersSeeder para insertar usuarios con todos los permisos

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Users as u;
+use App\Permissions as p;
 use Illuminate\Support\Facades\Crypt; 
 
 class UsersSeeder extends Seeder
@@ -13,7 +14,7 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        u::create([
+        $usuario = u::create([
             'nombres' => 'Jean carlos',
             'apellidos' => 'Contreras',
             'sexo' => 'Masculino',
@@ -23,5 +24,13 @@ class UsersSeeder extends Seeder
             'usuario' => 'jean',
             'password' => Crypt::encryptString('123')
         ]);
+        
+        $usuario->permisos()->detach();
+        $permisos = p::all();
+        $permisos = collect($permisos)->map(function($d) use($usuario){
+            return ['idPermiso' => $d['id'], 'idUsuario' => $usuario['id']];
+        });
+       
+        $usuario->permisos()->attach($permisos);
     }
 }
